8bc1d6747d3b2bec79b81b5063a601d213cf76a8
sanskar <sanskarvns19@gmail.com>
2025-12-25 13:14:18 +0530

    Book store challange

--- functional_programming/src/main/java/sanskar/com/functional_programming/intermediate/BookStoreChallenge.java
+++ functional_programming/src/main/java/sanskar/com/functional_programming/intermediate/BookStoreChallenge.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
 import java.util.stream.Stream;
 
 public class BookStoreChallenge {
