9eee4efa2c2c55d3b647c01da784d0440a98156a
sanskar <sanskarvns19@gmail.com>
2025-12-26 13:55:33 +0530

    optional

--- functional_programming/src/main/java/sanskar/com/functional_programming/optional/NPEDemo.java
+++ functional_programming/src/main/java/sanskar/com/functional_programming/optional/NPEDemo.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class NPEDemo {
     public static void main(String[] args) {
-        String name = "sanskar" ;
+        String name = "sanska" ;
 //        traditional
        if(name!=null){
            System.out.println(name.length());
